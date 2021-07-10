c3ba1c1956c892ef6b64e65d4f6465dff981f39a
charloq <carlosn.quinteros@gmail.com>
2021-07-09 22:45:34 -0300

    Se agrega modelo y base.sql

---
+++ Source/BaseDatos.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Data.OleDb
+
+Public Class BaseDatos
+
+    Public Conexion As New OleDbConnection
+    Public Comando As New OleDbCommand
+    Public Adaptador As New OleDbDataAdapter
+    Public DS As DataSet
+    Public DR As OleDbDataReader
+    Public CadenaDeConexion As String = "Provider = Microsoft.ACE.OLEDB.12.0;Data Source=Libreria.accdb"
+
+    Private Function GetDataSet(query As String) As DataSet
+        Try
+            Conexion.ConnectionString = CadenaDeConexion
+            Conexion.Open()
+
+            Comando.Connection = Conexion
+            Comando.CommandType = CommandType.Text
+            Comando.CommandText = query
+
+            Adaptador = New OleDbDataAdapter(Comando)
+            DS = New DataSet
+            Adaptador.Fill(DS)
+
+        Catch
+            MessageBox.Show(ErrorToString)
+        Finally
+            Conexion.Close()
+        End Try
+        Return DS
+    End Function
+
+    Public Function LoginUsuario(nombreUsuario As String, contraseña As String) As Boolean
+        Dim query = $"Select * from Usuarios Where nombreusuario='{nombreUsuario}' and contraseña='{contraseña}'"
+        Dim tabla = Me.GetDataSet(query).Tables(0)
+        If tabla.Rows.Count > 0 Then
+            Return True
+        End If
+        Return False
+    End Function
+End Class
---
+++ Source/Modelo.vb
@@ -0,0 +1,114 @@
+﻿
+Public Class Persona
+
+    Public Property Id As Integer
+    Public Property Nombre As String
+    Public Property Apellido As String
+    Public Property DNI As String
+    Public Property FechaNacimiento As Date
+    Public Property Email As String
+    Public Property Telefono As String
+    Public Property Direccion As String
+    Public Property Ciudad As String
+
+    Public Sub New()
+
+    End Sub
+End Class
+
+Public Class Usuario
+    Inherits Persona
+    Public Property NombreUsuario As String
+    Public Property Contraseña As String
+    Public Property Permisos As Permiso()
+    Public Property Estado As Estado
+
+    Public Sub New()
+        MyBase.New()
+    End Sub
+End Class
+
+Public Class Permiso
+
+    Public Property Id As Integer
+    Public Property Nombre As String
+    Public Property Descripcion As String
+
+    Public Sub New()
+
+    End Sub
+End Class
+
+Public Enum Estado
+    Activo
+    Inactivo
+    Bloqueado
+    Baja
+End Enum
+
+
+Public Class Reserva
+
+    Public Property Id As Integer
+
+    Public FechaInicio As Date
+
+    Public FechaFinal As Salon
+
+    Public Sub New()
+
+    End Sub
+End Class
+
+Public Class Salon
+
+    Public Property Id As Integer
+    Public Property Nombre As String
+    Public Property Reservas As Reserva()
+
+    Public Sub New()
+
+    End Sub
+
+End Class
+
+Public Class EstadoCuenta
+
+    Public Property Id As Integer
+    Public Property CuponesPago As CuponPago()
+    Public Property Moroso As Boolean
+
+    Public Sub New()
+
+    End Sub
+End Class
+
+
+Public Class CuponPago
+    Public Property Id As Integer
+    Public Property Monto As Decimal
+    Public Property Referencia As String
+    Public Property Email As String
+    Public Property Fecha As Date
+
+    Public Sub New()
+
+    End Sub
+
+End Class
+
+Public Class Jugador
+    Inherits Persona
+    Public Property Deporte As Deporte
+
+    Public Sub New()
+        MyBase.New()
+    End Sub
+End Class
+
+Public Enum Deporte
+    Basquet = 1
+    Patin = 2
+    DanzaArabe = 3
+    ComediaMusical = 4
+End Enum
